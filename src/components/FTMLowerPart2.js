a4610b32a45b7e7bda88ef97b94ec52d36ecf2fa
vladkondakov <vladisloove.kondakov@gmail.com>
2019-04-09 22:26:59 +0800

    Back to preveous folder

--- src/components/FTMLowerPart2.js
+++ src/components/FTMLowerPart2.js
@@ -1,83 +1,90 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import CurrentDirectory from './CurrentDirectory';
 import FoldersList from './FoldersList';
 import FilesList from './FilesList';
 import load from '../utils/load';
 
 class FTMLowerPart2 extends React.Component {
-    state = {
-        fullStructure: [],
-        currentDir: '',
-        requestAddress: 'http://cris.icc.ru/fm2?id=',
-    };
+  state = {
+    lastFolder: '',
+    fullStructure: [],
+    currentDir: '',
+    requestAddress: 'http://cris.icc.ru/fm2?id='
+  };
 
-    componentDidMount() {
-        const fS = this.getFolderStructure(this.props.user);
+  componentDidMount() {
+    const fS = this.getFolderStructure(this.props.user);
 
-        this.setState({
-            currentDir: `S:/users/${this.props.user}`,
-            fullStructure: fS
-        });
+    this.setState({
+      currentDir: `S:/users/${this.props.user}`,
+      fullStructure: fS
+    });
+  }
+
+  handleClickFolder = folder => {
+    //sadd if condition
+    const folderStructure = this.getFolderStructure(folder.attr.id);
+
+    this.setState({
+      lastFolder: folder.attr.id,
+      fullStructure: folderStructure,
+      currentDir: folder.attr.originalPath
+    });
+  };
+
+  handleClickItem = (item, event) => {
+    event.preventDefault();
+
+    if (item.attr.rel !== 'folder') {
+      this.setState({
+        currentDir: item.attr.originalPath
+      });
+      return;
     }
 
-    handleClickFolder = folder => {
-        const folderStructure = this.getFolderStructure(folder.attr.id);
+    this.handleClickFolder(item);
+  };
 
-        this.setState({
-            fullStructure: folderStructure,
-            currentDir: folder.attr.originalPath
-        });
-    };
+  getFolderStructure = id => {
+    // Вызов метода из модуля, где написан get - запрос
+    return load(`${this.state.requestAddress}${id}`);
+  };
 
-    handleClickItem = (item, event) => {
-        event.preventDefault();
+  render() {
+    const { fullStructure, currentDir } = this.state;
 
-        if (item.attr.rel !== 'folder') {
-            this.setState({
-                currentDir: item.attr.originalPath
-            });
-            return;
-        }
+    return (
+      <div className="jumbotron">
+        <CurrentDirectory currentDir={currentDir} />
+        {this.renderStructure(fullStructure)}
+      </div>
+    );
+  }
 
-        this.handleClickFolder(item);
-    };
+  renderStructure = structure => {
+    let structureFiles = [];
+    let structureFolders = [];
 
-    getFolderStructure = id => {
-        // Вызов метода из модуля, где написан get - запрос
-        return load(`${this.state.requestAddress}${id}`);
-
-    };
-
-    render() {
-        const { fullStructure, currentDir } = this.state;
-
-        return (
-            <div className="jumbotron">
-                <CurrentDirectory currentDir={currentDir} />
-                {this.renderStructure(fullStructure)}
-            </div>
-        );
-    }
-
-    renderStructure = structure => {
-        let structureFiles = [];
-        let structureFolders = [];
-
-        structure.forEach(function (curr, index, initial_array) {
-            if (curr.attr.rel === 'folder') {
-                structureFolders.push(curr);
-            } else {
-                structureFiles.push(curr);
-            }
-        });
-        return (
-            <div>
-                <FoldersList structure={structureFolders} onClick={this.handleClickItem} />
-                <FilesList structure={structureFiles} onClick={this.handleClickItem} />
-            </div>
-        );
-    }
+    structure.forEach(function(curr, index, initial_array) {
+      if (curr.attr.rel === 'folder') {
+        structureFolders.push(curr);
+      } else {
+        structureFiles.push(curr);
+      }
+    });
+    return (
+      <div>
+        <FoldersList
+          lastFolderID={this.state.lastFolder}
+          structure={structureFolders}
+          onClick1={this.handleClickItem}
+          onClick2={this.getFolderStructure}
+        />
+        <FilesList structure={structureFiles} onClick={this.handleClickItem} />
+      </div>
+    );
+  };
 }
 
 export default FTMLowerPart2;
--- src/components/FoldersList.js
+++ src/components/FoldersList.js
@@ -2,34 +2,42 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class FoldersList extends React.Component {
+  render() {
+    const { structure } = this.props.structure;
+    return <div className="jumbotron">{this.renderStructure(structure)}</div>;
+  }
 
-    render() {
-        const { structure } = this.props.structure;
-        return (
-            <div className="jumbotron">
-                {this.renderStructure(structure)}
-            </div>
-        );
-    }
+  renderItem = item => {
+    const { onClick1 } = this.props.onClick1;
+    return (
+      <div
+        className="card"
+        onClick={() => {
+          onClick1(item);
+        }}
+      >
+        <div className="body">{item.attr.data}</div>
+      </div>
+    );
+  };
 
-    renderItem = item => {
-        const { onClick } = this.props.onClick;
-        return (
-            <div className="card" onClick={() => {
-                onClick(item)
-            }}>
-                <div className="body">{item.attr.data}</div>
-            </div>
-        );
-    }
-
-    renderStructure = structure => {
-        return (
-            <div>
-                {structure.map(item => this.renderItem(item))}
-            </div>
-        )
-    };
+  renderStructure = structure => {
+    const { lastFolderID } = this.props.lastFolderID;
+    const { onClick2 } = this.props.onClick2;
+    return (
+      <div>
+        <div
+          className="card"
+          onClick={() => {
+            onClick2(lastFolderID);
+          }}
+        >
+          <div className="body">...</div>
+        </div>
+        <div>{structure.map(item => this.renderItem(item))}</div>
+      </div>
+    );
+  };
 }
 
 export default FoldersList;
